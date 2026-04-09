b997c7677fd2ed28211a70218c80ea6cc95641b2
Austin Hague <arhague302@gmail.com>
2026-04-09 16:21:21 -0500

    room 3 completed

---
+++ Assets/Scripts/Darryl's Scripts/KeypadUI.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using TMPro;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+
+
+public class KeypadUI : MonoBehaviour
+{
+    public GameObject panel;
+    public TMP_InputField inputField;
+    public TMP_Text feedbackText;
+    public GameObject door;
+
+    public string correctCode = "HELLO WORLD";
+
+    public enum KeypadState { Idle, Active, Correct, Incorrect }
+    public KeypadState currentState = KeypadState.Idle;
+
+    // public void OpenPanel()
+    // {
+    //     panel.SetActive(true);
+    //     inputField.text = "";
+    //     feedbackText.text = "";
+
+    //     Cursor.lockState = CursorLockMode.None;
+    //     Cursor.visible = true;
+
+    //     inputField.ActivateInputField();
+    // }
+
+    public void OpenPanel()
+    {
+        currentState = KeypadState.Active;
+
+        panel.SetActive(true);
+        inputField.text = "";
+        feedbackText.text = "";
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        inputField.ActivateInputField();
+        FindFirstObjectByType<FirstPersonController>().enabled = false;
+    }
+
+    public void ClosePanel()
+    {
+        panel.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        FindFirstObjectByType<FirstPersonController>().enabled = true;
+    }
+
+    // public void SubmitCode()
+    // {
+    //     string input = inputField.text.ToUpper();
+
+    //     if (input == correctCode)
+    // {
+    //     feedbackText.text = "ACCESS GRANTED";
+
+    //     if (door != null)
+    //     {
+    //         door.SetActive(false);
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("Door not assigned!");
+    //     }
+    // }
+
+    //     inputField.text = "";
+    //     inputField.ActivateInputField();
+    // }
+
+    public void SubmitCode()
+    {
+        string input = inputField.text.ToUpper();
+
+        if (input == correctCode)
+        {
+            currentState = KeypadState.Correct;
+            feedbackText.text = "ACCESS GRANTED";
+
+            if (door != null)
+                door.SetActive(false);
+        }
+        else
+        {
+            currentState = KeypadState.Incorrect;
+            feedbackText.text = "INVALID CODE";
+        }
+
+        inputField.text = "";
+        inputField.ActivateInputField();
+    }
+
+    void Update()
+{
+    if (panel.activeSelf && Keyboard.current.escapeKey.wasPressedThisFrame)
+    {
+        ClosePanel();
+    }
+}
+}
---
+++ Assets/Scripts/PanelClick_P1.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using State = panelState;
+
+public enum panelState
+{
+        IDLE,
+        ACTIVATED,
+        DISABLED
+}
+public class PanelClick_P1 : MonoBehaviour
+{
+    public State currentPanelState {get; private set;}
+    public int panelID;
+    public Fsm_Puzzle_1 manager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        currentPanelState = State.IDLE;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        switch (currentPanelState)
+        {   
+            // not touch the picture
+            case State.IDLE:
+                break;
+            // the correct order
+            case State.ACTIVATED:
+                break;
+            // puzzle 1 solved -> no more touch 
+            case State.DISABLED:
+                break;
+        }
+    }
+
+    public void ChangeState(State newState)
+    {
+        currentPanelState = newState;
+    }
+
+    // when click on the picture, call Grab function in the CustomCursor
+    public void Grab()
+    {
+        // if already solved, ignore it
+        if (currentPanelState == State.DISABLED) return;
+        //
+        if (manager == null) return; 
+
+        manager.PressPanel(this); // follow the panel that player just click 
+    }
+    public void SetActivated()
+    {
+         ChangeState(State.ACTIVATED);
+    }
+
+    public void Resetpanel()
+    {
+        ChangeState(State.IDLE);
+    }
+
+    public void SetDisabled()
+    {
+        ChangeState(State.DISABLED);
+    }
+}
