bcebf53ed7290c16a7e51b6a0a1870b3eb274e4b
iris1810 <153126022+iris1810@users.noreply.github.com>
2026-04-09 00:50:30 -0500

    Add Sounds

--- Assets/Scripts/Fsm_Puzzle_1.cs
+++ Assets/Scripts/Fsm_Puzzle_1.cs
@@ -4,7 +4,8 @@ using State = Puzzle1_State;
 public enum Puzzle1_State {
   IDLE,
   IN_PROCESS,
-  SOLVED
+  SOLVED,
+  
 }
 
 public class Fsm_Puzzle_1 : MonoBehaviour {
@@ -55,6 +56,7 @@ public class Fsm_Puzzle_1 : MonoBehaviour {
     if (PuzzleState == State.IDLE)
     {
       ChangeState(State.IN_PROCESS);
+      
     }
 
     //check for in order input panel
@@ -80,6 +82,8 @@ public class Fsm_Puzzle_1 : MonoBehaviour {
   {
     ChangeState(State.SOLVED);
 
+    SoundManager1.Play(SoundType1.CORRECT);
+
     //disable for each panel
     foreach(var panel in panels)
     {
@@ -96,6 +100,7 @@ public class Fsm_Puzzle_1 : MonoBehaviour {
   {
    currentStep =0;
    ChangeState(State.IDLE);
+   SoundManager1.Play(SoundType1.WRONG);
 
    foreach(var panel in panels)
     {
